7604474c6f2b7305aac2fa878cd4b87f43d23ee3
KJH <bkho@naver.com>
2024-11-25 20:18:15 +0900

    fix: nextconfig 수정

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { withContentlayer } from 'next-contentlayer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['three'], // 외부 패키지 파일을 트랜스파일대상에 포함시키기, threejs를 추가함으로 번들 사이즈를 줄일 수 있음
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   experimental: {
     mdxRs: true,
