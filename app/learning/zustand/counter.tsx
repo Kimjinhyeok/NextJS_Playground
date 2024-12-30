6ad91854b2fe862ba93e0892c2cee919619c5fc0
KJH <bkho@naver.com>
2024-12-30 16:11:38 +0900

    Fix: zustand 초기화 전 초기값 출력 안나오게 수정

--- app/learning/zustand/counter.tsx
+++ app/learning/zustand/counter.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useCountStore } from "@/app/store/zustand";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CountController from "./controller";
 
 export interface CounterProps {
@@ -9,15 +9,17 @@ export interface CounterProps {
 }
 export default function Counter({initialCount}:CounterProps) {
 
-  const count = useCountStore(state => state.count);
+  const {hasInit, count} = useCountStore();
+  
   useEffect(() => {
-    useCountStore.setState(({ count: initialCount }));
+    useCountStore.setState(({hasInit: true, count: initialCount}))
   }, [])
   
   return (
     <>
       <div className="w-full flex justify-center">
-        <h1 className="text-5xl">{ count }</h1>
+        <h1 className="text-5xl">{ hasInit ? count : initialCount }</h1>
+        {/* <h1 className="text-5xl">{ count }</h1> */}
       </div>
       <CountController />
     </>
--- app/store/zustand.ts
+++ app/store/zustand.ts
@@ -2,11 +2,13 @@ import { create } from 'zustand';
 
 interface NumberStates {
   count: number,
+  hasInit: boolean,
   increase: ()=>void,
   decrease: ()=>void,
 }
 export const useCountStore = create<NumberStates>( set => ({
   count: 0,
+  hasInit: false,
   increase: () => set( state => ({ count: state.count+1 })),
   decrease: () => set( state => ({ count: state.count-1 })),
 
