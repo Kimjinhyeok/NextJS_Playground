866b2506167cde51d4e000db71f4919ebb44c356
KJH <bkho@naver.com>
2024-12-30 18:06:51 +0900

    Add: Multi Object 형태의 State Store 등록

--- app/store/zustand.ts
+++ app/store/zustand.ts
@@ -13,3 +13,48 @@ export const useCountStore = create<NumberStates>( set => ({
   decrease: () => set( state => ({ count: state.count-1 })),
 
 }));
+
+export type ComplexState = {
+  hasInit: boolean,
+  dataA: {
+    value1: number,
+    value2: number,
+  },
+  name: string,
+}
+type ComplexCreateType = {
+
+  setState: (data:ComplexState)=>void
+  updateDataA: (key: keyof ComplexState['dataA'], value: number)=>void
+  setName: (name:string)=>void,
+} & ComplexState;
+
+export const useComplexStore = create<ComplexCreateType>( set => ({
+  hasInit: false,
+  dataA: {
+    value1: 0,
+    value2: -1
+  },
+  name: "",
+  setState(data) {
+    set(data);
+  },
+  updateDataA(key, value) {
+    set(state => ({
+      ...state,
+      dataA: {
+        ...state.dataA,
+        [key]: value
+      }
+    }))
+  },
+  setName(name) {
+    set(state => {
+      
+      return ({
+        ...state,
+        name: name,
+      })
+    })
+  },
+}))
